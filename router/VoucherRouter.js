50936365db46515eaafb6e2f615e9e4d4a4d6ee1
Hielmi Sulaeman <hielmisulaeman10@gmail.com>
2024-06-21 20:40:18 +0700

    fix: create file VoucherRouter.js that previously deleted

---
+++ router/VoucherRouter.js
@@ -0,0 +1,49 @@
+import express from "express";
+import {
+  addVoucher,
+  deleteVoucher,
+  getAllVouchers,
+  getVoucher,
+  updateVoucher,
+} from "../controllers/VoucherController.js";
+import { claimVoucer, unclaimVoucher } from "../controllers/ClaimController.js";
+import AuthMiddleware from "../middlewares/AuthMiddleware.js";
+import roleValidation from "../middlewares/RoleValidation.js";
+import UploadMiddleware from "../middlewares/UploadMiddleware.js";
+
+const voucherRouter = express.Router();
+
+// public route
+voucherRouter.get("/vouchers", getAllVouchers);
+
+// protected route
+voucherRouter.get("/voucher/:id", AuthMiddleware, getVoucher);
+voucherRouter.post(
+  "/voucher",
+  AuthMiddleware,
+  roleValidation("admin"),
+  UploadMiddleware.single("foto"),
+  addVoucher
+);
+
+voucherRouter.put(
+  "/voucher/:id",
+  AuthMiddleware,
+  roleValidation("admin"),
+  UploadMiddleware.single("foto"),
+  updateVoucher
+);
+
+voucherRouter.delete(
+  "/voucher/:id",
+  AuthMiddleware,
+  roleValidation("admin"),
+  deleteVoucher
+);
+
+// claim and unclaim voucher
+voucherRouter.get("/claim-vouchers", AuthMiddleware, claimVoucer);
+voucherRouter.post("/claim-voucher", AuthMiddleware, claimVoucer);
+voucherRouter.delete("/unclaim-voucher/:id", AuthMiddleware, unclaimVoucher);
+
+export default voucherRouter;
